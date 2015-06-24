dd00ed8d4c8b2493e90fd769460ef3d022bc85d1
Dario Oliveri <github.com/Darelbi>
2015-06-24 18:24:12 +0200

    added echo to travis to see what is wrong

--- cmake/EnableCpp11.cmake
+++ cmake/EnableCpp11.cmake
@@ -1,7 +1,7 @@
-if ($ENV{CMAKE_FORCE_OPTIONS_FOR} MATCHES Clang)
+if ( "$ENV{CMAKE_FORCE_OPTIONS_FOR}" MATCHES "Clang")
 	add_compile_options( -std=c++11 -Os -O3 -fexceptions )
 endif()
 
-if ($ENV{CMAKE_FORCE_OPTIONS_FOR} MATCHES GCC)
-  add_compile_options( -std=c++11 -Os -O3 -fexceptions )
+if ("$ENV{CMAKE_FORCE_OPTIONS_FOR}" MATCHES "GCC")
+	add_compile_options( -std=c++11 -Os -O3 -fexceptions )
 endif()
