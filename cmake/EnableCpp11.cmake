56eee33671ad70340b873e344b252bb58b136c04
Dario Oliveri <github.com/Darelbi>
2015-06-24 15:53:39 +0200

    [clang] make sure build use it: attemp 1

--- cmake/EnableCpp11.cmake
+++ cmake/EnableCpp11.cmake
@@ -1,3 +1,7 @@
+if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+	add_compile_options( -std=c++11 -Os -O3 -fexceptions )
+endif()
+
 if (MINGW)
   add_compile_options( -std=c++11 -Os -O3 -fexceptions )
 endif()
--- cmake/SuperStrictWarnings.cmake
+++ cmake/SuperStrictWarnings.cmake
@@ -1,3 +1,16 @@
+if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+	add_compile_options( -Woverloaded-virtual -Wctor-dtor-privacy -Wconversion
+                      -Wformat=2 -Winit-self -Winline -Wmissing-include-dirs
+                      -Wredundant-decls -Wshadow -Wstack-protector
+                      -Wstrict-null-sentinel -Wswitch-default -Winvalid-pch
+                      -Wstrict-aliasing=2 -Wunknown-pragmas -Wundef
+                      -Wpointer-arith -Wcast-qual -Wwrite-strings
+                      -Wvolatile-register-var -Wno-unused-parameter
+                      -Wno-variadic-macros -Wunreachable-code -Wcast-align
+                      #-Wzero-as-null-pointer-constant #commented because Cmake generate crappy code 
+					  -Wold-style-cast)
+endif()
+
 if(MINGW)
   add_compile_options( -Woverloaded-virtual -Wctor-dtor-privacy -Wconversion
                       -Wformat=2 -Winit-self -Winline -Wmissing-include-dirs
