65c880001ee616a16a2d4c654dd6a347a02126e0
Dario Oliveri <github.com/Darelbi>
2015-06-26 10:27:08 +0200

    Cmake: use old style cxx flags

--- cmake/SuperStrictWarnings.cmake
+++ cmake/SuperStrictWarnings.cmake
@@ -2,15 +2,6 @@ if ( MSVC)
 
 else()
 
-	add_compile_options( -Woverloaded-virtual -Wctor-dtor-privacy -Wconversion
-                      -Wformat=2 -Winit-self -Winline -Wmissing-include-dirs
-                      -Wredundant-decls -Wshadow -Wstack-protector
-                      -Wstrict-null-sentinel -Wswitch-default -Winvalid-pch
-                      -Wstrict-aliasing=2 -Wunknown-pragmas -Wundef
-                      -Wpointer-arith -Wcast-qual -Wwrite-strings
-                      -Wvolatile-register-var -Wno-unused-parameter
-                      -Wno-variadic-macros -Wunreachable-code -Wcast-align
-                      #-Wzero-as-null-pointer-constant #commented because Cmake generate crappy code 
-					  -Wold-style-cast)
+	SET( CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS}  -Woverloaded-virtual -Wctor-dtor-privacy -Wconversion -Wformat=2 -Winit-self -Winline     -Wmissing-include-dirs-Wredundant-decls -Wshadow -Wstack-protector -Wstrict-null-sentinel -Wswitch-default -Winvalid-pch -Wstrict-aliasing=2 -Wunknown-pragmas -Wundef -Wpointer-arith -Wcast-qual -Wwrite-strings -Wvolatile-register-var -Wno-unused-parameter -Wno-variadic-macros -Wunreachable-code -Wcast-align -Wold-style-cast")
 	
 endif()
