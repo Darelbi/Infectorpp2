e0a0f7c3f26442623a4733a6c392ac20756ff672
Dario Oliveri <github.com/Darelbi>
2015-07-18 01:27:25 +0200

    test self recursion (simple recursion)

---
+++ tests/selfRecursive.cpp
@@ -0,0 +1,43 @@
+/*******************************************************************************
+   Copyright (C) 2015 Dario Oliveri
+   See copyright notice in LICENSE.md
+*******************************************************************************/
+#include <InfectorContainer.hpp>
+#include <priv/ExceptionHandling.hpp>
+#undef NDEBUG
+#include <assert.h>
+#include "TestHelper.hpp"
+
+class SelfRecursive{
+	
+	public:
+	
+	SelfRecursive(std::unique_ptr<SelfRecursive>){}
+	
+	void method(){}
+	
+};
+
+int selfRecursive(int argc, char **){
+	
+	using namespace Infector;
+	
+	Container ioc;
+	
+	ioc.bindAsNothing< SelfRecursive> ();
+	
+	
+	try{
+		ioc.wire<  SelfRecursive, Unique<SelfRecursive> >();
+		assert(false); //test failed
+	}
+	catch( const priv::CircularDependencyEx &ex){
+		
+	}
+	catch(...){
+		assert(false); //test failed
+	}
+	
+	
+	return 0;
+}
